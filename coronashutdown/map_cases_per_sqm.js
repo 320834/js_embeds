6e92affe92407c5172cff9f9c2baf4aa5666ab35
320834 <justinchen1000@gmail.com>
2020-04-03 00:15:46 -0400

    update sq mile

--- coronashutdown/map_cases_per_sqm.js
+++ coronashutdown/map_cases_per_sqm.js
@@ -282,7 +282,7 @@ map.on("load", () => {
         " County" +
         "<br>" +
         e["features"][0]["properties"][selected_date] +
-        " Cases Per Ten-Thousand Capita " +
+        " Cases Per Square Mile" +
         "(" +
         display_date +
         ")";
