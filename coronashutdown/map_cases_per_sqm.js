97281857b2ab2ef5201b900d115a0b3e81133e0c
320834 <justinchen1000@gmail.com>
2020-07-03 20:43:14 -0400

    updated month list

--- coronashutdown/map_cases_per_sqm.js
+++ coronashutdown/map_cases_per_sqm.js
@@ -73,7 +73,19 @@ function format_date_display(dateObj) {
     month = "May";
   } else if (month == 6) {
     month = "June";
-  }
+  } else if (month == 7) {
+    month = "Jul";
+  } else if (month == 8) {
+    month = "Aug";
+  } else if (month == 9) {
+    month = "Sept";
+  } else if (month == 10) {
+    month = "Oct";
+  } else if (month == 11) {
+    month = "Nov";
+  } else if (month == 12) {
+    month = "Dec";
+  } 
   return month + "\t" + date;
 }
 
